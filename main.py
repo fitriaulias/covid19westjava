bdc71ca4aeba4f607f06a5222d626f232bb1f4ee
fitriaulias <fitriauliasanni@gmail.com>
2021-12-06 06:54:06 +0700

    menambah detail setelah ekstraksi data

---
+++ main.py
@@ -0,0 +1,78 @@
+"""
+Aplikasi Sebaran COVID-19 di Jawa Barat
+"""
+
+
+def ekstraksi_data():
+    pass
+
+
+"""
+TOTAL TERKONFIRMASI
+Jawa Barat: 708.043
+ 27
+Indonesia
+4.257.685
+ 196
+ISOLASI/DALAM PERAWATAN
+Jawa Barat
+923
+ -37
+Indonesia
+7.526
+ -106
+SELESAI ISOLASI/SEMBUH
+Jawa Barat
+692.383
+ 64
+Indonesia
+4.106.292
+ 298
+MENINGGAL
+Jawa Barat
+14.737
+ 0
+Indonesia
+143.867
+ 4
+KONTAK ERATⓘ
+572.979
+Total Kontak Erat
+25.744
+(4.49%)
+Masih Dikarantina
+547.235
+(95.51%)
+Discardedⓘ
+SUSPEKⓘ
+216.886
+Total Suspek
+8.568
+(3.95%)
+Isolasi/Dalam Perawatan
+208.318
+(96.05%)
+Discardedⓘ
+PROBABLEⓘ
+6.410
+Total Probable
+276
+(4.31%)
+Isolasi/Dalam Perawatan
+3.999
+(62.39%)
+Selesai Isolasi/Sembuh
+2.135
+(33.31%)
+Meninggal
+"""
+
+
+def tampilkan_data(result):
+    pass
+
+
+if __name__ == '__main__':
+    print('Aplikasi utama')
+    result = ekstraksi_data()
+    tampilkan_data(result)
